4a58842fea12b4acdc9b8cdcc7d8a81bd50f6576
Ninad Kuralkar <ninad.kuralkar2@gmail.com>
2025-09-05 01:42:04 +0530

    Update text_to_speech_page.dart

--- text_to_speech_page.dart
+++ text_to_speech_page.dart
@@ -1,1 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 
+class TextToSpeechPage extends StatefulWidget {
+  const TextToSpeechPage({super.key});
+
+  @override
+  State<TextToSpeechPage> createState() => _TextToSpeechPageState();
+}
+
+class _TextToSpeechPageState extends State<TextToSpeechPage> {
+  final FlutterTts tts = FlutterTts();
+  final TextEditingController controller = TextEditingController();
+
+  void speak() async {
+    if (controller.text.isNotEmpty) {
+      await tts.speak(controller.text);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("🗣️ Text to Speech"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextField(
+              decoration: const InputDecoration(
+                labelText: 'Enter text to speak',
+                border: OutlineInputBorder(),
+                hintText: 'Type something here...',
+                labelStyle: TextStyle(fontSize: 18, color: Colors.deepPurple),
+                hintStyle: TextStyle(fontSize: 16, color: Colors.grey),
+              ),
+              controller: controller,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.deepPurple,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              onPressed: speak,
+              child: const Text(
+                "Speak",
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
